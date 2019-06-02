a2adbfb700e7dcc97899020fafbcfe4f62adc9b6
Matthias Hölzl <tc@xantira.com>
2019-06-03 01:39:50 +0200

    Fix dependency problems, improve build file

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,12 +7,19 @@ repositories {
     jcenter()
 }
 
+val hamcrestVersion = "2.1"
+val jUnitVersion = "5.4.2"
+val slf4jVersion = "1.7.25"
+val textIoVersion = "3.2.0"
+
 dependencies {
-    compile("org.beryx:text-io:3.2.0")
-    testCompile("org.junit.jupiter:junit-jupiter-api:5.4.2")
-    testCompile("org.junit.jupiter:junit-jupiter-params:5.4.2")
-    testImplementation("org.hamcrest:hamcrest-library:2.1")
-    testRuntime("org.junit.jupiter:junit-jupiter-engine:5.4.2")
+    compile("org.beryx:text-io:$textIoVersion")
+    compile("org.slf4j:slf4j-api:$slf4jVersion")
+    compile("org.slf4j:slf4j-simple:$slf4jVersion")
+    testCompile("org.junit.jupiter:junit-jupiter-api:$jUnitVersion")
+    testCompile("org.junit.jupiter:junit-jupiter-params:$jUnitVersion")
+    testImplementation("org.hamcrest:hamcrest-library:$hamcrestVersion")
+    testRuntime("org.junit.jupiter:junit-jupiter-engine:$jUnitVersion")
 }
 
 java {
